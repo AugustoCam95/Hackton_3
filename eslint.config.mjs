a3f859adc3c49883713eaf770ba84631225fb7d4
AugustoCam95 <augustocam95@gmail.com>
2024-07-01 11:14:42 -0300

    up eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "react/react-in-jsx-scope": "off", // Disable the rule for React in JSX scope
+    },
+  },
   pluginJs.configs.recommended,
   pluginReactConfig,
 ];
